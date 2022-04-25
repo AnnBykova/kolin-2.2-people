e7e2e7fd66b0e4ebf661c866f21472bb9a6549a6
AnnBykova <ann_karelia>
2022-04-25 17:12:43 +0300

    Add variant with 11

--- src/main/kotlin/People.kt
+++ src/main/kotlin/People.kt
@@ -1,13 +1,15 @@
-
 fun main() {
     printLikes(1)
     printLikes(10)
     printLikes(32)
     printLikes(61)
+    printLikes(11)
+    printLikes(10111)
+    printLikes(121)
 }
 
 fun peopleName(likes: Int): String {
-    return if (likes == 1 || likes % 10 == 1) "человеку" else "людям"
+    return if (likes % 10 == 1 && likes % 100 != 11 && likes != 11) "человеку" else "людям"
 }
 
 fun printLikes(likes: Int) {
